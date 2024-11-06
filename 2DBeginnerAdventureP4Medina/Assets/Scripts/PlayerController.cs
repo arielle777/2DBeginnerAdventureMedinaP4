3001aec5234e121b210dc826cce03be2eae9e73c
arielle777 <723653@student.dusd.net>
2024-11-05 19:23:14 -0800

    I finished the animations
    finished the animations and began the world interactions;.

--- 2DBeginnerAdventureP4Medina/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP4Medina/Assets/Scripts/PlayerController.cs
@@ -9,18 +9,19 @@ public class PlayerControllerTutorialUpdates : MonoBehaviour
     
     public float speed = 8.0f;
     public int maxHealth = 5;
-    public float timeInvicible = 2;
+    public float timeInvicible = 2.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
     bool isInvincible;
     float invincibleTimer;
-
-   
     
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
     public InputAction LeftAction;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
     
     // Start is called before the first frame update
     void Start()
@@ -28,6 +29,7 @@ public class PlayerControllerTutorialUpdates : MonoBehaviour
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
         currentHealth = 5;
+        animator = GetComponent < Animator>();
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 10;
     }
@@ -38,6 +40,16 @@ public class PlayerControllerTutorialUpdates : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
@@ -60,6 +72,8 @@ public class PlayerControllerTutorialUpdates : MonoBehaviour
     {
         if(amount < 0 )
         {
+            animator.SetTrigger("Hit");
+
             if (isInvincible)
             {
                 return;
---
+++ 2DBeginnerAdventureP4Medina/Assets/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    RigidBody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
